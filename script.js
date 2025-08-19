d33da0d3caadd78b304b87a583cb0172ed7c8449
Galin <122453213+Ethertale@users.noreply.github.com>
2025-08-19 09:53:03 +0300

    Feature/random color (#9)
    * add random hex color
    
    * new random
    
    * new random 2

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.getElementById('apply-btn').addEventListener('click', function () {
   var c = document.getElementById('color-input').value;
   document.getElementById('box').style.backgroundColor = c;
 });
+
+function randomHexColor() {
+  var random = random;
+  // new random here
+  // new random 2
+}

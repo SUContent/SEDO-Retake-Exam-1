8842bec79fd634e5b6fb5f669101d0fddb66b9ea
Galin Yanakov <galin@ivel.in>
2025-08-19 09:47:46 +0300

    refactor 2

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ document.getElementById('apply-btn').addEventListener('click', function () {
   var c = document.getElementById('color-input').value;
   document.getElementById('box').style.backgroundColor = c;
   // refactoring here added
+  // refactoring 2 added
 });
